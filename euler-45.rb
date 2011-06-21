8cf2ed1dba88a23e9b9685c7258df4849632ec93
Alex Duller <alexduller@gmail.com>
2011-06-21 19:48:54 +0100

    Solved Euler 45. Woot.

--- euler-45.rb
+++ euler-45.rb
@@ -1,10 +1,31 @@
-n = 284
-while 1
-  p n if n % 10000 == 0
-  t = n * (n+1) / 2
-  p = n * (3*n - 1) / 2
-  h = n * (2*n - 1)
-  break if t == p && p == h
+n = 286
+p = 164
+h = 142
+
+while true
+  tn = n * (n + 1) / 2
+  p n if n % 1000 == 0
+
+  (p..n).each do |i|
+    pn = i * ((3*i) - 1) / 2
+    if pn > tn
+      p = i
+      break
+    end
+    if pn == tn
+      puts "pn == tn"
+      (h..i).each do |j|
+        hn = j * ((2*j) - 1)
+        if pn > tn
+          h = i
+          break
+        end
+        if hn == pn
+          puts "Answer: #{tn}"
+          exit
+        end
+      end
+    end
+  end
   n += 1
 end
-p t
