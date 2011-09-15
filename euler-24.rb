8db4f890606cc6304b38573a20df4dc8e885a1f8
Alex Duller <alexd@picochip.com>
2011-09-15 16:43:24 +0100

    Solved Euler 24

--- euler-24.rb
+++ euler-24.rb
@@ -1,8 +1,23 @@
-digits = [0,1,2]
+digits = [0,1,2,3,4,5,6,7,8,9]
 
-digits.each_with_index do |digit|
-  puts digit
-  other_digits = digits.clone
-  other_digits.delete(digit)
-  p other_digits
+def getPermutations(arr)
+  if arr.length == 1
+    return arr
+  end
+
+  currentPermutations = []
+  arr.each do |digit|
+    others = arr.clone
+    others.delete(digit)
+    getPermutations(others).each do |permutation|
+      currentPermutations << (digit.to_s + permutation.to_s)
+    end
+  end
+
+  currentPermutations
 end
+
+p getPermutations(digits)[999999]
+
+# 0 1
+# 1 0
