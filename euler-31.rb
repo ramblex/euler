abf4b96ad1c8704fde71a80965198ca39198d0ef
Alex Duller <alexduller@gmail.com>
2012-03-28 00:48:16 +0100

    Solved Euler 31

--- euler-31.rb
+++ euler-31.rb
@@ -7,12 +7,24 @@
 # 1£1 + 150p + 220p + 15p + 12p + 31p
 # How many different ways can £2 be made using any number of coins?
 # 
-coins = [1, 2, 5, 10, 20, 50, 100, 200].reverse
+@coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
-remaining = 200
-coins.each do |coin|
-  remaining -= coin
-  until remaining > 0
-    
+@num_solutions = 0
+
+def find_solutions(remaining, partial)
+  if remaining == 0
+    @num_solutions += 1
+    return
   end
+
+  solutions = []
+  @coins.each do |coin|
+    if coin <= remaining and (partial.empty? or partial.last <= coin)
+      solutions << find_solutions(remaining - coin, partial + [coin])
+    end
+  end
+  solutions
 end
+
+find_solutions(200, [])
+p @num_solutions
